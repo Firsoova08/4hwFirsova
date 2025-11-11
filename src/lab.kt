dac7a4757241977d89b648a09f740d3823066aff
Firsoova08 <foxalishca@mail.ru>
2025-11-11 22:26:06 +0300

    2 задание

--- src/lab.kt
+++ src/lab.kt
@@ -1,9 +1,16 @@
 fun showGameLogo() {
     println("=== EPIC QUEST ===")
 }
+fun showEnemy(name: String, level: Int) {
+    println("Враг: $name (Уровень: $level)")
+}
+
 
 fun main() {
     //1
     showGameLogo()
     showGameLogo()
+    //2
+    showEnemy("Дракон", 15)
+    showEnemy("Вампир", 10)
 }
